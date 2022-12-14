72bf22494a4c020602add37d19e6977831be4b67
cjm0611 <81799492+cjm0611@users.noreply.github.com>
2022-12-14 11:48:18 +0900

    feat: 사용자 숫자 입력 예외처리 기능

--- src/constant/GameSetting.js
+++ src/constant/GameSetting.js
@@ -1,5 +1,7 @@
 const BASEBALL_CONSTANT = Object.freeze({
   length: 3,
+  max: 9,
+  min: 1,
 });
 
 module.exports = { BASEBALL_CONSTANT };
--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,11 +1,47 @@
 const { Console } = require('@woowacourse/mission-utils');
 const { GAME_INFORMATION } = require('../constant/Message');
+const { BASEBALL_CONSTANT } = require('../constant/GameSetting');
 
 const InputView = {
   readNumber(callback) {
     Console.readLine(GAME_INFORMATION.input, (input) => {
+      this.handleExceptionWrongInput(input);
       callback(input);
     });
+  },
+
+  handleExceptionWrongInput(number) {
+    this.handleExceptionNotNumber(number);
+    this.handleExceptionWrongLength(number);
+    this.handleExceptionOutOfRange(number);
+    this.handleExceptionDeplication(number);
+  },
+
+  handleExceptionNotNumber(number) {
+    if (Number.isNaN(Number(number))) {
+      throw '숫자만 입력해주세요';
+    }
+  },
+
+  handleExceptionWrongLength(number) {
+    if (number.length !== 3) {
+      throw '세 자리의 숫자를 입력해주세요';
+    }
+  },
+
+  handleExceptionOutOfRange(number) {
+    [...number].forEach((digit) => {
+      if (digit < BASEBALL_CONSTANT.min || digit > BASEBALL_CONSTANT.max) {
+        throw '각 자리의 숫자는 1~9만 허용합니다';
+      }
+    });
+  },
+
+  handleExceptionDeplication(number) {
+    const numberSet = new Set(number);
+    if (number.length !== numberSet.size) {
+      throw '각 자리의 숫자는 모두 달라야 합니다';
+    }
   },
 };
 module.exports = InputView;
